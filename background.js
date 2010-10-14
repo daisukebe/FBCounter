27f8d1359cc00af750664dd1dbbd4aecdc91fa61
daisukebe <poleon.kd@gmail.com>
2010-10-14 18:15:20 -0400

    divide share and counter

---
+++ background.js
@@ -0,0 +1,53 @@
+/**
+ * Add number to the browser icon
+ **/
+var addNum = function(num, tab){
+    if(num > 9999) {
+        var z = "";
+        for(var i = 0; i < String(num).length - 4; i++)
+            z += "0";
+        num = String(num)[0] + z + "K";
+        chrome.browserAction.setBadgeText({tabId: tab.id, text:String(num)});
+    }else if(num > 0)
+        chrome.browserAction.setBadgeText({tabId: tab.id, text:String(num)});
+    else chrome.browserAction.setBadgeText({tabId: tab.id, text:""});
+};
+
+/**
+ * When a page is complete to load, get the number of 
+ * Share/Like on the page.
+ **/
+chrome.tabs.onUpdated.addListener(function(tabId, inf, tab) {
+    if(inf.status === "complete"){
+        (function(url){
+            var l1 = "http://api.facebook.com/restserver.php?method=links.getStats&urls=";
+            var l2 = "&format=json";
+	    
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", l1 + encodeURIComponent(url) + l2, true);
+            xhr.onload = function(){
+                var re = JSON.parse(xhr.responseText);
+                var s = re[0].share_count;
+                //var l = re[0].like_count;
+                //var c = re[0].comment_count;
+                //var t = re[0].total_count;
+		addNum(s, tab);
+            }
+            xhr.send();
+        })(tab.url);
+    }
+    
+});
+
+/**
+ * When you click the browser icon, a new window will
+ * appear and you can share the page with your friend.
+ **/
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.windows.create({
+        url: "http://www.facebook.com/sharer.php?u=" +
+	    encodeURIComponent(tab.url) + "&t=" + tab.title,
+        height: 500,
+        width: 600,
+        type: "popup"});
+});
